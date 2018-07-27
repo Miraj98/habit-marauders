05d9ef469a319b38f7ec409fe52b57401b826388
Miraj <mirajholmes@gmail.com>
2018-07-27 22:12:36 +0530

    Cleaned up the contract code a bit

--- contracts/HabitContract.sol
+++ contracts/HabitContract.sol
@@ -9,25 +9,27 @@ contract HabitContract is AvatarContract {
         string _description,
         uint _periodicity,
         uint _totalSessions) internal {
-            
-        avatars[avatar_id_of[msg.sender]].Habits[avatars[avatar_id_of[msg.sender]].number_of_habits] = Habit(_name, _description, _periodicity, _totalSessions, 0);
-        avatars[avatar_id_of[msg.sender]].number_of_habits++;
+        Avatar storage avatar = avatars[avatar_id_of[msg.sender]];   
+        avatar.Habits[avatar.number_of_habits] = Habit(_name, _description, _periodicity, _totalSessions, 0);
+        avatar.number_of_habits++;
         
     }
 
     function getHabit( uint habitId) external view returns(string,string,uint,uint,uint) {
-        string storage _title = avatars[avatar_id_of[msg.sender]].Habits[habitId].title;
-        string storage _description = avatars[avatar_id_of[msg.sender]].Habits[habitId].description;
-        uint _periodicity = avatars[avatar_id_of[msg.sender]].Habits[habitId].periodicity;
-        uint _totalSessions = avatars[avatar_id_of[msg.sender]].Habits[habitId].totalSessions;
-        uint _sessionsCompleted = avatars[avatar_id_of[msg.sender]].Habits[habitId].sessionsCompleted;
+        Avatar storage avatar = avatars[avatar_id_of[msg.sender]];
+        string storage _title = avatar.Habits[habitId].title;
+        string storage _description = avatar.Habits[habitId].description;
+        uint _periodicity = avatar.Habits[habitId].periodicity;
+        uint _totalSessions = avatar.Habits[habitId].totalSessions;
+        uint _sessionsCompleted = avatar.Habits[habitId].sessionsCompleted;
         return (_title,_description,_periodicity,_totalSessions,_sessionsCompleted);
     }
 
     function deleteHabit(uint habitId) external {
-        avatars[avatar_id_of[msg.sender]].Habits[habitId] = avatars[avatar_id_of[msg.sender]].Habits[avatars[avatar_id_of[msg.sender]].number_of_habits-1];
-        delete avatars[avatar_id_of[msg.sender]].Habits[avatars[avatar_id_of[msg.sender]].number_of_habits-1];
-        avatars[avatar_id_of[msg.sender]].number_of_habits--;
+        Avatar storage avatar = avatars[avatar_id_of[msg.sender]];
+        avatar.Habits[habitId] = avatar.Habits[avatar.number_of_habits-1];
+        delete avatar.Habits[avatar.number_of_habits-1];
+        avatar.number_of_habits--;
 
     }
 
